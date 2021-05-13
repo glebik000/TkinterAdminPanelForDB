95c39676d8bba3f7f2b48bdf739f0cb55dfa2a56
SliceNDice14 <alexkillzone53@gmail.com>
2021-05-13 23:55:01 +1000

    Update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from functools import partial
 import tkinter as ui
 from database import *
 
-def handler(name):
+def handler(name, cur, conn):
     window = ui.Tk()
     window.rowconfigure(1, weight=1,minsize=5)
     my_frame = ui.Frame(master=window, 
@@ -13,10 +13,26 @@ def handler(name):
         label = ui.Label(master=my_frame,
                 text="dialogue text",
                 width=10)
+
+        que =   """ INSERT INTO dialogues (dialogue_text)
+                    VALUES(?)
+                """
+        stri = ('test text 1',)
+        cur.execute(que, stri)
+        conn.commit() 
+
     elif name == "theory":
         label = ui.Label(master=my_frame,
                 text="theory text",
                 width=10)
+
+        que =   """ INSERT INTO theory (theory_text)
+                    VALUES(?)
+                """
+        stri = ('just a test',)
+        cur.execute(que, stri)
+        conn.commit() 
+
     elif name == "questions":
         label = ui.Label(master=my_frame,
                 text="question text",
@@ -24,7 +40,7 @@ def handler(name):
     elif name == "languages":
         label = ui.Label(master=my_frame,
                 text="language name",
-                width=10)
+                width=15)
     elif name == "correct":
         label = ui.Label(master=my_frame,
                 text="is correct",
@@ -40,7 +56,10 @@ def handler(name):
 
 class start_window:
     conn = create_connection()
+    create_table(conn)
     table_names = ("dialogues", "theory", "questions", "languages", "correct", "answers")
+    #print("checking conn: ", conn)
+    cur = conn.cursor()
     window = ui.Tk()
     for i in range(6):
         window.rowconfigure(i, weight=1,minsize=5)
@@ -51,7 +70,7 @@ class start_window:
         button = ui.Button(master=my_frame,
                 text=f"{table_names[i]}", 
                 width=25,
-                command=partial(handler, table_names[i])
+                command=partial(handler, table_names[i], cur, conn)
                 )
         button.pack(padx=5, pady=5)
     window.mainloop()
